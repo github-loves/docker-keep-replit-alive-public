0e29cc2a305b4b960960132e02e841d50e44347e
github-loves <84881581+github-loves@users.noreply.github.com>
2022-12-29 10:51:28 +0800

    Update main.js

--- main.js
+++ main.js
@@ -2,7 +2,7 @@ var http = require('http')
 var request = require('request');
 
 // 这里填你的replit应用的url，多个url用逗号隔开
-let replit_app_urls = ["https://V2RAY.eva43.repl.co"]
+let replit_app_urls = [""]
 
 var port = 4000
 var server = http.createServer(function (request, response) {
